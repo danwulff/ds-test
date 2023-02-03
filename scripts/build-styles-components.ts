239967cea645cfd79fbdd3e08bf9d67ca924affd
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-03 21:17:52 +0000

    basic files for each package, some scripts completed

---
+++ scripts/build-styles-components.ts
@@ -0,0 +1,8 @@
+// builds everything that will end up being a node package pushed up to github packages
+// import { execSync } from "child_process";
+// import { readdirSync } from "fs";
+
+// build styles
+// TODO
+
+// build components
