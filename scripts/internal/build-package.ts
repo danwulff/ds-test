b761d500e12b833daefb3e73605cbb53cf2ab660
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-04 00:09:39 +0000

    builds working

--- scripts/internal/build-package.ts
+++ scripts/internal/build-package.ts
@@ -1,7 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
 export function buildPackage(parentDir: string, name: string) {
-  var _a;
   // template package
   const tempPackage = JSON.parse(
     readFileSync(`${parentDir}/ds-package.json`, "utf-8").replaceAll(
@@ -15,9 +14,7 @@ export function buildPackage(parentDir: string, name: string) {
   );
   // add properties from module package
   tempPackage.version = modulePackage.version;
-  (_a = tempPackage.peerDependencies) !== null && _a !== void 0
-    ? _a
-    : (tempPackage.peerDependencies = {});
+  tempPackage.peerDependencies ??= {};
   Object.entries(modulePackage.peerDependencies || {}).forEach(
     ([key, value]) => {
       tempPackage.peerDependencies[key] = value;
