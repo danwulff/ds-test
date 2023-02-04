38d4a705858b3618fe65c8a9dbe327960e433e2b
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-04 21:49:12 +0000

    Phase 1 (#4)
    * Update version to be in sync, fix publish script
    
    * update icon

--- components/ds-icon/icon.ts
+++ components/ds-icon/icon.ts
@@ -3,7 +3,11 @@ import { customElement, property } from "lit/decorators.js";
 
 @customElement("ds-icon")
 export class DSIcon extends LitElement {
-  static override styles = css``;
+  static override styles = css`
+    span {
+      color: grey;
+    }
+  `;
 
   @property()
   text = "some-icon";
