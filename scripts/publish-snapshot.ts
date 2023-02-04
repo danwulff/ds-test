aff1a47b5ecbaddc7a73262c081bc4ec77ca757e
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-04 14:01:15 +0000

    publish snapshot on pr update

---
+++ scripts/publish-snapshot.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from "fs";
+import { execSync } from "child_process";
+import { getPackageNames } from "./internal/get-package-names";
+
+const [_exec, _path, pullRequestNum, commitSHA] = process.argv;
+
+// deploy style package snapshot
+const stylesDir = "styles";
+const stylePackageNames = getPackageNames(stylesDir);
+stylePackageNames.forEach((name) => {
+  // set package version
+  const currentVersion = JSON.parse(
+    readFileSync(`${stylesDir}/${name}/dist/package.json`, "utf-8")
+  ).version;
+  const newVersion = `${currentVersion}-pr${pullRequestNum}-${commitSHA}`;
+  execSync(`npm version ${newVersion}`, { stdio: "inherit" });
+  // publish snapshot
+  execSync(`npm publish`, { stdio: "inherit" });
+});
+
+// deploy component packages
+const componentsDir = "components";
+const componentPackageNames = getPackageNames(componentsDir);
+componentPackageNames.forEach((name) => {
+  // set package version
+  const currentVersion = JSON.parse(
+    readFileSync(`${componentsDir}/${name}/dist/package.json`, "utf-8")
+  ).version;
+  const newVersion = `${currentVersion}-pr${pullRequestNum}-${commitSHA}`;
+  execSync(`npm version ${newVersion}`, { stdio: "inherit" });
+  // publish snapshot
+  execSync(`npm publish`, { stdio: "inherit" });
+});
