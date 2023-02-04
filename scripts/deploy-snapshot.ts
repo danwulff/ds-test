6a48a4a6895d228ba06545a84b1a22c0ddfa1197
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-04 13:42:59 +0000

    test sha

---
+++ scripts/deploy-snapshot.ts
@@ -0,0 +1,12 @@
+// import { getPackageNames } from "./internal/get-package-names";
+
+const [exec, _path, commitSHA] = process.argv;
+console.log("SHA", commitSHA);
+
+// deploy style package snapshot
+// const stylesDir = "styles";
+// const stylePackageNames = getPackageNames(stylesDir);
+
+// deploy component packages
+// const componentsDir = "components";
+// const componentPackageNames = getPackageNames(componentsDir);
