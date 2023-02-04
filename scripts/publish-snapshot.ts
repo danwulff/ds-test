9ebaec8566d8697be21e438772b28ea302908b17
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-04 14:16:39 +0000

    Fix version and publish directory

--- scripts/publish-snapshot.ts
+++ scripts/publish-snapshot.ts
@@ -5,35 +5,37 @@ import { getPackageNames } from "./internal/get-package-names";
 const [_exec, _path, pullRequestNum, commitSHA] = process.argv;
 
 // set git user to make "npm version" happy
-execSync(`git config user.email "danielwulff.ee@gmail.com"`, {
-  stdio: "inherit",
-});
-execSync(`git config user.name "Daniel Wulff"`, { stdio: "inherit" });
+// execSync(`git config user.email "danielwulff.ee@gmail.com"`, {
+//   stdio: "inherit",
+// });
+// execSync(`git config user.name "Daniel Wulff"`, { stdio: "inherit" });
 
 // deploy style package snapshot
 const stylesDir = "styles";
 const stylePackageNames = getPackageNames(stylesDir);
 stylePackageNames.forEach((name) => {
+  const distDir = `${stylesDir}/${name}/dist`;
   // set package version
   const currentVersion = JSON.parse(
-    readFileSync(`${stylesDir}/${name}/dist/package.json`, "utf-8")
+    readFileSync(`${distDir}/package.json`, "utf-8")
   ).version;
   const newVersion = `${currentVersion}-pr${pullRequestNum}-${commitSHA}`;
-  execSync(`npm version ${newVersion}`, { stdio: "inherit" });
+  execSync(`cd ${distDir} && npm version ${newVersion}`, { stdio: "inherit" });
   // publish snapshot
-  execSync(`npm publish`, { stdio: "inherit" });
+  execSync(`cd ${distDir} && npm publish`, { stdio: "inherit" });
 });
 
 // deploy component packages
 const componentsDir = "components";
 const componentPackageNames = getPackageNames(componentsDir);
 componentPackageNames.forEach((name) => {
+  const distDir = `${componentsDir}/${name}/dist`;
   // set package version
   const currentVersion = JSON.parse(
-    readFileSync(`${componentsDir}/${name}/dist/package.json`, "utf-8")
+    readFileSync(`${distDir}/package.json`, "utf-8")
   ).version;
   const newVersion = `${currentVersion}-pr${pullRequestNum}-${commitSHA}`;
-  execSync(`npm version ${newVersion}`, { stdio: "inherit" });
+  execSync(`cd ${distDir} && npm version ${newVersion}`, { stdio: "inherit" });
   // publish snapshot
-  execSync(`npm publish`, { stdio: "inherit" });
+  execSync(`cd ${distDir} && npm publish`, { stdio: "inherit" });
 });
