ab0de04e8db7d821a9bf8b9d4d2bbb90a02a57dd
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-06 11:28:03 +0000

    Update merge script with helper functions

--- scripts/internal/publish-package.ts
+++ scripts/internal/publish-package.ts
@@ -3,14 +3,11 @@ import { execSync } from "child_process";
 export function publishPackage(
   name: string,
   parentDir: string,
-  newVersion: "patch" | "minor" | "major" | string,
   tag: string
 ): void {
   console.log(`Publishing @danwulff/${name} changes...`);
   const distDir = `${parentDir}/${name}/dist`;
-  execSync(`cd ${distDir} && npm version ${newVersion}`, { stdio: "inherit" });
   execSync(`cd ${distDir} && npm publish --tag ${tag}`, {
     stdio: "inherit",
   });
-  console.log(`Published ${name} ${newVersion} with tag ${tag}`);
 }
