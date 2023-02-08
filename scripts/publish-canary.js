5f0ee1df24c78b62ca54a39d4fc9684189c885c9
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-08 20:50:09 +0000

    fix issue with canary publish

--- scripts/publish-canary.js
+++ scripts/publish-canary.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
-const [_exec, _path, labels, pullRequestNum] = process.argv;
+const [_exec, _path, labels, pullRequestNum, commitSha] = process.argv;
 
 const parsedSemverLabels = JSON.parse(labels).filter(
   (label) => label === 'patch' || label === 'minor' || label === 'major'
@@ -12,6 +12,8 @@ if (parsedSemverLabels.length !== 1) {
 }
 
 const version = parsedSemverLabels[0];
-const prPrefix = `pr${ pullRequestNum }`;
+const distTag = `pr${ pullRequestNum }`;
+// Note: ideally preId and distTag would be the same but this issue exists: https://github.com/lerna/lerna/issues/3065
+const preId = `${distTag}.${commitSha}`
 
-execSync(`lerna publish --canary "${ version }" --preid "${ prPrefix }" --dist-tag "${ prPrefix }" --yes`, { stdio: 'inherit' });
+execSync(`lerna publish --canary "${ version }" --preid "${ preId }" --dist-tag "${ distTag }" --yes`, { stdio: 'inherit' });
