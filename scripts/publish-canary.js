d10cd4b7cf4d60b9ff6731f543c2f39e3d65f0c5
Daniel Wulff <danielwulff.ee@gmail.com>
2023-02-08 12:42:15 +0000

    Add PR automation

---
+++ scripts/publish-canary.js
@@ -0,0 +1,17 @@
+import { execSync } from 'child_process';
+
+const [_exec, _path, labels, pullRequestNum] = process.argv;
+
+const parsedSemverLabels = JSON.parse(labels).filter(
+  (label) => label === 'patch' || label === 'minor' || label === 'major'
+);
+
+if (parsedSemverLabels.length !== 1) {
+  console.log('Unexpected number of labels. Please add "patch", "minor" or "major" label to the PR.');
+  process.exit(1);
+}
+
+const version = parsedSemverLabels[0];
+const prPrefix = `pr${ pullRequestNum }`;
+
+execSync(`lerna publish --canary "${ version }" --preid "${ prPrefix }" --dist-tag "${ prPrefix }"`, { stdio: 'inherit' });
